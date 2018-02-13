dee10ef371dcc874b0b075f434cd228d6184e52e
wpaulk <paulk.wil@gmail.com>
2018-02-13 12:51:51 -0500

    ready to deploy

--- server.js
+++ server.js
@@ -30,7 +30,12 @@ app.use(express.static(__dirname + '/public'));
 
 // ** Database **//
 // set up database connection
-mongoose.connect('mongodb://localhost/multivision');
+if(env === 'development') {
+    mongoose.connect('mongodb://localhost/multivision');
+}
+else {
+    mongoose.connect('mongodb://wpaulk:multivision@ds235418.mlab.com:35418/multivision');
+}
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
@@ -57,6 +62,6 @@ app.get('*', function(req, res) {
 });
 
 // ** Startup **//
-let port = 3030;
+let port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
